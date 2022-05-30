d4376cdcd979bc49bab1aca5395b8cfc2f73049a
Amitdhal007 <amit0162.be21@chitkara.edu.in>
2022-05-30 19:18:52 +0530

    Adding Files

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void myfunction(string name ,int age){
+    cout<<name<<" Bhai"<<" of Age "<<age<<endl;
+    }
+
+int main(){
+    myfunction("Anish",20);
+
+    // myfunction();
+    return 0;
+}
+
